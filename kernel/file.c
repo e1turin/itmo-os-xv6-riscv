aa1ca5fbbaeaf689824c55dad181e421796527a5
e1turin <ivturnik2003@gmail.com>
2023-11-05 21:58:24 +0300

    Part 1. File allocations are fixed, first test is passed

--- kernel/file.c
+++ kernel/file.c
@@ -16,7 +16,7 @@
 struct devsw devsw[NDEV];
 struct {
   struct spinlock lock;
-  struct file file[NFILE];
+  // struct file file[NFILE];
 } ftable;
 
 void
@@ -29,17 +29,26 @@ fileinit(void)
 struct file*
 filealloc(void)
 {
-  struct file *f;
+  // struct file *f;
+  //
+  // acquire(&ftable.lock);
+  // for(f = ftable.file; f < ftable.file + NFILE; f++){
+  //   if(f->ref == 0){
+  //     f->ref = 1;
+  //     release(&ftable.lock);
+  //     return f;
+  //   }
+  // }
+  // release(&ftable.lock);
+  // return 0;
 
-  acquire(&ftable.lock);
-  for(f = ftable.file; f < ftable.file + NFILE; f++){
-    if(f->ref == 0){
-      f->ref = 1;
-      release(&ftable.lock);
-      return f;
-    }
+  struct file *f = (struct file*) bd_malloc(sizeof(struct file));
+
+  if (f != 0) {
+    f->ref = 1;
+    return f;
   }
-  release(&ftable.lock);
+
   return 0;
 }
 
@@ -59,7 +68,27 @@ filedup(struct file *f)
 void
 fileclose(struct file *f)
 {
-  struct file ff;
+  // struct file ff;
+  //
+  // acquire(&ftable.lock);
+  // if(f->ref < 1)
+  //   panic("fileclose");
+  // if(--f->ref > 0){
+  //   release(&ftable.lock);
+  //   return;
+  // }
+  // ff = *f;
+  // f->ref = 0;
+  // f->type = FD_NONE;
+  // release(&ftable.lock);
+  //
+  // if(ff.type == FD_PIPE){
+  //   pipeclose(ff.pipe, ff.writable);
+  // } else if(ff.type == FD_INODE || ff.type == FD_DEVICE){
+  //   begin_op();
+  //   iput(ff.ip);
+  //   end_op();
+  // }
 
   acquire(&ftable.lock);
   if(f->ref < 1)
@@ -68,18 +97,17 @@ fileclose(struct file *f)
     release(&ftable.lock);
     return;
   }
-  ff = *f;
-  f->ref = 0;
-  f->type = FD_NONE;
   release(&ftable.lock);
 
-  if(ff.type == FD_PIPE){
-    pipeclose(ff.pipe, ff.writable);
-  } else if(ff.type == FD_INODE || ff.type == FD_DEVICE){
+  if(f->type == FD_PIPE){
+    pipeclose(f->pipe, f->writable);
+  } else if(f->type == FD_INODE || f->type == FD_DEVICE){
     begin_op();
-    iput(ff.ip);
+    iput(f->ip);
     end_op();
   }
+
+  bd_free(f);
 }
 
 // Get metadata about file f.
